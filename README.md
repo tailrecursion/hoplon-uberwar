424445037be29b175a010cda0d2b0fc9d9260517
Micha Niskin <micha.niskin@gmail.com>
2014-01-20 18:14:58 -0500

    workin

--- README.md
+++ README.md
@@ -4,7 +4,31 @@ A Clojure library designed to ... well, that part is up to you.
 
 ## Usage
 
-FIXME
+Build uberwar for production:
+
+```
+$ boot war
+```
+
+Build Hoplon for development (needs 2 terminals):
+
+```
+# in one shell
+$ boot watch hoplon
+
+# in the other shell
+$ boot dev
+```
+
+The `dev` task will pop a REPL in the `hello-world.core` namespace. From there
+you can start a local Jetty server:
+
+```clojure
+hello-world.core=> (def server (dev-server))
+2014-01-20 18:13:11.968:INFO:oejs.Server:jetty-7.6.8.v20121106
+2014-01-20 18:13:12.005:INFO:oejs.AbstractConnector:Started SelectChannelConnector@0.0.0.0:33333
+#'hello-world.core/server
+```
 
 ## License
 
--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,12 +1,13 @@
 (ns hello-world.core
   (:gen-class)
   (:require
-    [ring.middleware.resource        :refer [wrap-resource]]
-    [ring.middleware.session         :refer [wrap-session]]
-    [ring.middleware.session.cookie  :refer [cookie-store]]
-    [ring.middleware.file-info       :refer [wrap-file-info]]
-    [tailrecursion.castra.handler    :refer [castra]]))
-
+   [ring.adapter.jetty              :refer [run-jetty]]
+   [ring.middleware.resource        :refer [wrap-resource]]
+   [ring.middleware.session         :refer [wrap-session]]
+   [ring.middleware.session.cookie  :refer [cookie-store]]
+   [ring.middleware.file            :refer [wrap-file]]
+   [ring.middleware.file-info       :refer [wrap-file-info]]
+   [tailrecursion.castra.handler    :refer [castra]]))
 
 (def app
   (->
@@ -15,7 +16,13 @@
     (wrap-resource "public")    
     (wrap-file-info)))
 
-
+(defn dev-server [& [port]]
+  (->
+    (castra  'hello-world.api)
+    (wrap-session {:store (cookie-store {:key "a 16-byte secret"})})
+    (wrap-file "resources/public")
+    (wrap-file-info)
+    (run-jetty {:join? false :port (or port 33333)})))
 
 (defn foo
   "I don't do a whole lot."
