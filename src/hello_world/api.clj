a87b15b8224e06af3502166c50acbb3b61e80217
Micha Niskin <micha.niskin@gmail.com>
2014-05-21 08:52:54 -0400

    Slight mods to app, nothing major

--- src/hello_world/api.clj
+++ src/hello_world/api.clj
@@ -1,9 +1,7 @@
 (ns hello-world.api
-  (:refer-clojure :exclude [defn])
-  (:require [hello-world.rules :refer [allow deny]])
-  (:require [tailrecursion.castra :refer [defn]])
-)
+  (:require [hello-world.rules :refer [allow]])
+  (:require [tailrecursion.castra :refer [defrpc]]))
 
-(defn doit []
+(defrpc doit []
   {:rpc/pre [(allow)]}
-  "This came from the server.")
+  (java.util.Date.))
